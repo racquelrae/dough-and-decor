9b9a45e30bd839633070780a46aba3c08e503d5e
racquelbeebe <144067135+racquelrae@users.noreply.github.com>
2025-07-04 12:20:29 -0700

    Reinitialize repo in correct root folder

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBD6eP8K8nUKyfJ3DSxDbtQnBWWTyKz5Zc",
+  authDomain: "dough-and-decor.firebaseapp.com",
+  projectId: "dough-and-decor",
+  storageBucket: "dough-and-decor.firebasestorage.app",
+  messagingSenderId: "582729680515",
+  appId: "1:582729680515:web:3008dc1db284c2f2653db6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

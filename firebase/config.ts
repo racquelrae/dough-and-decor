089ca960bca72bbb46924dff30fb3790abb7b650
racquelbeebe <144067135+racquelrae@users.noreply.github.com>
2025-08-10 17:03:17 -0700

    refactor: update Firebase configuration to use environment variables and add dotenv dependency

--- firebase/config.ts
+++ firebase/config.ts
@@ -2,15 +2,18 @@ import { getAuth } from "firebase/auth"
 import { getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
+import Constants from 'expo-constants';
+
+const extra = Constants.expoConfig?.extra || {};
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBD6eP8K8nUKyfJ3DSxDbtQnBWWTyKz5Zc",
-  authDomain: "dough-and-decor.firebaseapp.com",
-  projectId: "dough-and-decor",
-  storageBucket: "dough-and-decor.firebasestorage.app", 
-  messagingSenderId: "582729680515",
-  appId: "1:582729680515:web:3008dc1db284c2f2653db6"
+  apiKey: extra.firebaseApiKey,
+  authDomain: extra.firebaseAuthDomain,
+  projectId: extra.firebaseProjectId,
+  storageBucket: extra.firebaseStorageBucket,
+  messagingSenderId: extra.firebaseMessagingSenderId,
+  appId: extra.firebaseAppId,
 };
 
 // Initialize Firebase only once
